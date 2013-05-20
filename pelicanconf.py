46a7e5a790e77b1665f6af8d10e64f67f6f98d7e
numahell <e.helly@gmail.com>
2013-05-20 23:57:53 +0300

    Config: modify lanyrd url

--- pelicanconf.py
+++ pelicanconf.py
@@ -52,7 +52,7 @@ SOCIAL = (
           ('Identica', 'identica', 'http://identi.ca/group/toulibre'),
           ('Twitter', 'twitter', 'https://twitter.com/toulibreorg'),
           ('Google+', 'google', 'https://plus.google.com/b/109128243242581226442/109128243242581226442/posts'),
-          #~ ('Lanyrd', 'lanyrd', 'http://lanyrd.com/2013/capitole-du-libre/'),
+          ('Lanyrd', 'lanyrd', 'http://lanyrd.com/2013/capitole-du-libre/'),
          )
 
 TWITTER_USERNAME = 'Toulibreorg'
