e79c9bf9964d5c1d00d1a0a0ddd4a290b71fe23c
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2013-06-06 21:56:21 +0200

    Add ical plugin and calendar in programm

--- pelicanconf.py
+++ pelicanconf.py
@@ -50,9 +50,9 @@ LINKS =  (
 
 # Social widget
 SOCIAL = (
-          ('Identica', 'identica', 'http://identi.ca/toulibreorg'),
+          ('Identica', 'identica', 'http://identi.ca/group/toulibre'),
           ('Twitter', 'twitter', 'https://twitter.com/toulibreorg'),
-          ('Google+', 'google', 'https://plus.google.com/+toulibre'),
+          ('Google+', 'google', 'https://plus.google.com/b/109128243242581226442/109128243242581226442/posts'),
           ('Lanyrd', 'lanyrd', 'http://lanyrd.com/2013/capitole-du-libre/'),
          )
 
--- src/pages/programme.rst
+++ src/pages/programme.rst
@@ -2,6 +2,8 @@
 Programme
 =========
 
+:calendar: src/files/capitole-du-libre-schedule.ics
+
 Retrouvez bientôt les conférences et ateliers du Capitole du Libre et des événements hébergés.
 
 Si vous le souhaitez, l'`appel à conférences et ateliers`_ pour le Capitole du Libre 2013 est lancé, vous pouvez faire vos propositions jusqu'au **15 septembre 2013**. Le programme sera en ligne vers le 7 octobre. 
