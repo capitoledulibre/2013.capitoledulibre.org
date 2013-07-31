df08ab5683a431df0488af7190053868f1dcde9f
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2013-07-31 23:36:58 +0200

    Google plus link

--- pelicanconf.py
+++ pelicanconf.py
@@ -52,7 +52,7 @@ LINKS =  (
 SOCIAL = (
           ('Identica', 'identica', 'http://identi.ca/toulibreorg'),
           ('Twitter', 'twitter', 'https://twitter.com/toulibreorg'),
-          ('Google+', 'google', 'https://plus.google.com/b/109128243242581226442/109128243242581226442/posts'),
+          ('Google+', 'google', 'https://plus.google.com/+toulibre'),
           ('Lanyrd', 'lanyrd', 'http://lanyrd.com/2013/capitole-du-libre/'),
          )
 
