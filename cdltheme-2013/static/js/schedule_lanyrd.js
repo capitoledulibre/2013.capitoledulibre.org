22fc7cd3a860c49cf5fc7a6121ff50f3bede2bd9
Sebastien Dinot <sebastien@dinot.net>
2013-11-04 23:53:16 +0100

    Corrected date format

--- cdltheme-2013/static/js/schedule_lanyrd.js
+++ cdltheme-2013/static/js/schedule_lanyrd.js
@@ -18,7 +18,7 @@ function from_lanyrd_json(day, space) {
                 var d = new Date(sessions.sessions[i].start_time_epoch * 1000);
                 var m = moment([d.getFullYear(), d.getMonth() + 1, d.getDate(),
                                 d.getHours(), d.getMinutes(), d.getSeconds()])
-                sessions.sessions[i].start_time = m.format('HH[h]ss');
+                sessions.sessions[i].start_time = m.format('HH[h]mm');
             }
             else {
                 sessions.sessions[i].start_time = "??h??";
