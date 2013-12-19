a7c49a0661ba0505ab6ae5fd8d7ae7fb88ef1a44
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2013-12-20 00:02:47 +0100

    Add link to lua

--- pelicanconf.py
+++ pelicanconf.py
@@ -88,4 +88,5 @@ VIDEO_PAGES = [{'url': 'conferences/grand-public/','title':'Grand Public'},
                     {'url': 'conferences/techniques/','title':'Technique'},
                     {'url': 'conferences/akademy-fr/','title':'Akademy-fr'},
                     {'url': 'conferences/francejs/','title':'FranceJS'},
+                    {'url': 'conferences/lua-workshop/','title':'Lua workshop'},
                     ]
